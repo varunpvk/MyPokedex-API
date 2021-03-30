93d5958b1728fd0440bfad2f2dfb15d6d8fade17
Varun Palavalasa <varun.palavalasa@farfetch.com>
2021-03-30 17:44:28 +0100

    ApplicationServices: Implement Basic and Translated Pokemon Feature

---
+++ MyPokedex.ApplicationServices/Features/TranslatedPokemonFeature.cs
@@ -0,0 +1,43 @@
+﻿namespace MyPokedex.ApplicationServices.Features
+{
+    using MyPokedex.Core.DTOs;
+    using MyPokedex.Infrastructure.FunTranslationsClient;
+    using System;
+    using System.Threading.Tasks;
+
+    public class TranslatedPokemonFeature : ITranslatedPokemonFeature
+    {
+        private readonly ITranslationsService translationsService;
+        private readonly IBasicPokemonFeature basicPokemonFeature;
+        private const string caveText = "cave";
+
+        public TranslatedPokemonFeature(IBasicPokemonFeature basicPokemonFeature, ITranslationsService translationsService)
+        {
+            this.translationsService = translationsService;
+            this.basicPokemonFeature = basicPokemonFeature;
+        }
+
+        public async Task<PokemonInfoDto> GetTranslatedPokemonInfoAsync(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+                throw new ArgumentNullException("param: name cannot be null or empty");
+
+            string translatedValue = string.Empty;
+            var pokemonInfoDto = await this.basicPokemonFeature.GetBasicPokemonInfoAsync(name).ConfigureAwait(false);
+
+            if (isCaveOrLegendary(pokemonInfoDto)) {
+                var result = await this.translationsService.GetYodaTranslationAsync(pokemonInfoDto.Description).ConfigureAwait(false);
+                translatedValue = result.Content.TranslatedText ?? result.Content.OriginalText;
+            }
+            else {
+                var result = await this.translationsService.GetShakespheareTranslationAsync(pokemonInfoDto.Description).ConfigureAwait(false);
+                translatedValue = result.Content.TranslatedText ?? result.Content.OriginalText;
+            }
+
+            pokemonInfoDto.Description = translatedValue;
+            return pokemonInfoDto;
+        }
+
+        private bool isCaveOrLegendary(PokemonInfoDto pokemonInfo) => pokemonInfo.Habitat.Equals(caveText) || pokemonInfo.IsLegendary;
+    }
+}
